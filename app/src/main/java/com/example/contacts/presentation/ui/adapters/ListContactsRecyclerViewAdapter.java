7df63fd2883af7bada8d1d8b0f51768d39330928
7777slon <sounddaylight@gmail.com>
2020-09-18 01:48:16 +0300

    Code refactoring;

--- app/src/main/java/com/example/contacts/presentation/ui/adapters/ListContactsRecyclerViewAdapter.java
+++ app/src/main/java/com/example/contacts/presentation/ui/adapters/ListContactsRecyclerViewAdapter.java
@@ -3,7 +3,7 @@ package com.example.contacts.presentation.ui.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -42,9 +42,9 @@ public class ListContactsRecyclerViewAdapter extends RecyclerView.Adapter<ListCo
     public void onBindViewHolder(@NonNull ListContactsRecyclerViewAdapter.ViewHolder holder, int position) {
         final Contact contact = namesList.get(position);
         holder.tvNameContact.setText(contact.getName());
-        holder.imgDeleteContact.setOnClickListener(v -> fragment.deleteContact(contact));
-        holder.imgInfoContact.setOnClickListener(v -> fragment.openInfoScreen(contact));
-        holder.imgCallContact.setOnClickListener(v -> fragment.openCallScreen(contact));
+        holder.btnCallContact.setOnClickListener(v -> fragment.openCallScreen(contact));
+        holder.btnInfoContact.setOnClickListener(v -> fragment.openInfoScreen(contact));
+        holder.btnDeleteContact.setOnClickListener(v -> fragment.deleteContact(contact));
     }
 
     @Override
@@ -54,14 +54,14 @@ public class ListContactsRecyclerViewAdapter extends RecyclerView.Adapter<ListCo
 
     static class ViewHolder extends RecyclerView.ViewHolder {
         TextView tvNameContact;
-        ImageView imgCallContact, imgInfoContact, imgDeleteContact;
+        ImageButton btnCallContact, btnInfoContact, btnDeleteContact;
 
         ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvNameContact = itemView.findViewById(R.id.tvNameContact);
-            imgCallContact = itemView.findViewById(R.id.imgCallContact);
-            imgInfoContact = itemView.findViewById(R.id.imgInfoContact);
-            imgDeleteContact = itemView.findViewById(R.id.imgDeleteContact);
+            btnCallContact = itemView.findViewById(R.id.btnCallContact);
+            btnInfoContact = itemView.findViewById(R.id.btnInfoContact);
+            btnDeleteContact = itemView.findViewById(R.id.btnDeleteContact);
         }
     }
 }
